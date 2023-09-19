fb6f849bb4a3d010d2fcc08d8a8fa50e46e936ed
Darrle Perez <94949171+dperez626@users.noreply.github.com>
2023-09-19 14:29:26 -0400

    Add files via upload

--- chapter8_Functions.py
+++ chapter8_Functions.py
@@ -133,6 +133,27 @@ def build_person(first_name, last_name):
 musician = build_person('jimi', 'hendrix')
 print(musician)
 
+#greeter.py with a break
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted."""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+#This is not an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    print("(enter 'q' at any time to quit)")
+
+    f_name = input("First name: ")
+    if f_name == 'q':
+        break
+    l_name = input("Last name: ")
+    if l_name == 'q':
+        break
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}!")
+
 def build_person(first_name, last_name, age=None):
     """Return a dictionary """
     person = {'first': first_name, 'last': last_name}
@@ -143,20 +164,6 @@ def build_person(first_name, last_name, age=None):
 musician = build_person('jimi', 'hendrix', age=27)
 print(musician)
 
-#greeter.py
-def get_formatted_name(first_name, last_name):
-    """Return a full name, neatly formatted."""
-    full_name = f"{first_name} {last_name}"
-    return full_name.title()
-
-#This is an infinite loop!
-while True:
-    print("\nPlease tell me your name:")
-    f_name = input("First name: ")
-    l_name = input("Last name: ")
-
-    formatted_name = get_formatted_name(f_name, l_name)
-    print(f"\nHello, {formatted_name}!")
 
 #greeter.py with a break
 def get_formatted_name(first_name, last_name):
@@ -164,7 +171,7 @@ def get_formatted_name(first_name, last_name):
     full_name = f"{first_name} {last_name}"
     return full_name.title()
 
-#This is an infinite loop!
+#This is not an infinite loop!
 while True:
     print("\nPlease tell me your name:")
     print("(enter 'q' at any time to quit)")
@@ -180,5 +187,98 @@ while True:
     print(f"\nHello, {formatted_name}!")
 
 #8-6 City Names
+def city_country(city, county):
+    both = f"{city.title()}, {county.title()}"
+    return both
 
+location = city_country('belgrade', 'Serbia')
+print(location)
+location = city_country('bogota', 'colombia')
+print(location)
+location = city_country('Hamburg', 'germany')
+print(location)
 
+#8-7 Album
+def make_album(artist_name, album_title):
+    album = {'Artist': artist_name.title(), 'Title': album_title.title()}
+    return album
+
+music = make_album('mastodon', 'hushed and grim')
+print(music)
+music = make_album('bad bunny', 'un verano sin ti')
+print(music)
+music = make_album('2 cellos', 'let there be cello')
+print(music)
+
+#8-7 pt.2 Album
+def make_album(artist_name, album_title, number_songs = None):
+    album = {'Artist': artist_name.title(), 'Title': album_title.title()}
+    if number_songs:
+        album['Number of Songs'] = number_songs
+    return album
+
+music = make_album('mastodon', 'hushed and grim')
+print(music)
+music = make_album('bad bunny', 'un verano sin ti')
+print(music)
+music = make_album('2 cellos', 'let there be cello')
+print(music)
+music = make_album("coheed & cambria", "good apollo I'm burning star IV volume one: from fear through the eyes of madness", 15)
+print(music)
+
+#8-8 User Albums
+def make_album(artist_name, album_title):
+    album = {'Artist': artist_name.title(), 'Title': album_title.title()}
+    return album
+
+while True:
+    print("\nPlease enter a album:")
+    print("(Enter q at anytime to quit.)")
+
+    album_artist = input("Artist Name: ")
+    if album_artist == 'q':
+        break
+    album_name = input("Album Title: ")
+    if album_name == 'q':
+        break
+
+    formatted_album = make_album(album_artist, album_name)
+    print(formatted_album)
+
+music = make_album('mastodon', 'hushed and grim')
+print(music)
+music = make_album('bad bunny', 'un verano sin ti')
+print(music)
+music = make_album('2 cellos', 'let there be cello')
+print(music)
+
+#greet_users.py
+def greet_users(names):
+    """Print a simpler greeting to each user in the list."""
+    for name in names:
+        msg = f"Hello, {name.title()}!"
+        print(msg)
+
+usernames = ['hannah', 'ty', 'margot']
+greet_users(usernames)
+
+#printing_models.py
+#Start with some designs that need to be printed.
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+
+#Simulate printing each design, until none are left.
+# Move each design to completed_models after printing.
+while unprinted_designs:
+    current_design = unprinted_designs.pop()
+    print(f"Printing model: {current_design}")
+    completed_models.append(current_design)
+
+# Display all completed models.
+print("\nThe following models have been printed:")
+for completed_model in completed_models:
+    print(completed_model)
+
+#Organized greet_user.py
+def print_models(unprinted_designs, completed_models):
+    """ Simulate printing eacg design, until """
